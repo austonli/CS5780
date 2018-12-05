f13ac339fa22ced726bae2871e92dbc6ef9dd15b
Auston Li <al884@cornell.edu>
2018-12-05 15:59:21 -0500

    added some benchmarks

--- adjusted_rf.R
+++ adjusted_rf.R
@@ -2,17 +2,21 @@ setwd("C:/Users/meaus/OneDrive/4th Year/CS 5780/CS5780")
 
 train<- read.csv("mod_train.csv",header = TRUE) #reading the training data
 test<- read.csv("mod_test.csv",header = TRUE) #reading the test data
+og_train <- read.csv("train.csv",header = TRUE)
 og_test <- read.csv("test.csv",header = TRUE)
 
 library(dplyr)
 library(randomForest)
 library(Boruta)
 
-train_ft <- select(train, "favoriteCount","id","retweetCount","chars","month","year","day","time","label")
-test_ft <- select(test, "favoriteCount","id","retweetCount","chars","month","year","day","time")
-features <- select(train, "favoriteCount","id","retweetCount","chars","month","year","day","time")
+train_ft <- select(train, "favoriteCount","id","retweetCount","chars","month","year","day","time","label","X.s","isLink","atPerson","isRT")
+test_ft <- select(test, "favoriteCount","id","retweetCount","chars","month","year","day","time","X.s","isLink","atPerson","isRT")
+features <- select(train, "favoriteCount","id","retweetCount","chars","month","year","day","time","X.s","isLink","atPerson","isRT")
 
 rf <- randomForest(features, as.factor(train_ft$label), ntree=100, importance=TRUE)
 pred <- data.frame(id = og_test$id)
 pred$label <- predict(rf, test_ft)
-write.csv(pred, "random_forest_benchmark_2.csv")
+write.csv(pred, "random_forest_benchmark_4.csv")
+
+train_prd <-data.frame(id = og_train$id)
+train_prd <- predict(rf,train_ft)
--- rf.R
+++ rf.R
@@ -9,9 +9,29 @@ names(test)
 
 #install.packages("dplyr") **needs to be run the first time to install the package)
 library(dplyr)
+library(randomForest)
+library(Boruta)
 #isRetweet,retweeted, screenName,favorited, truncated, replytoSN are all irrelevant
 #replyToSN, created, longitude, latitude,replytoUID is mostly NAs
+#statusSource is missing from test
 
-filtered <- select(train, "id","text","favoriteCount","id.1","statusSource","screenName","retweetCount","label")
-features <- select(train, "id","text","favoriteCount","id.1","statusSource","screenName","retweetCount")
+filtered <- select(train, "id","favoriteCount","id.1","screenName","retweetCount","label")
+testfil <-  select(test, "id","favoriteCount","id.1","screenName","retweetCount")
+features <- select(train, "id","favoriteCount","id.1","screenName","retweetCount")
 lbls <- select(train,"label")
+
+
+bor.results <- Boruta(train,train$label,maxRuns=101,doTrace=0)
+
+
+rf <- randomForest(features, as.factor(lbls$label), ntree=100, importance=TRUE)
+pred <- data.frame(id = test$id)
+pred$label <- predict(rf, testfil)
+write.csv(pred, "random_forest_benchmark.csv")
+
+#remove NA values
+#remove columns or impute NA values for the ones that are mostly NAs
+#figure out how to use statusSource
+#split the time into time of day and date columns
+#diffuse the correlation between favoriteCount and retweet counts
+#parse the text of the tweets (word count feature engineering)
